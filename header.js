59c655da43fbaefc75e36654e65f3dafdf7fcc33
muditagrawal2007 <muditagrawal9415369961@gmail.com>
2026-03-31 11:30:22 +0530

    api_fetch

---
+++ header.js
@@ -0,0 +1,8 @@
+let root   = document.getElementById("root") 
+let header =document.createElement("div") 
+
+
+header.className = "heading"
+header.innerText = "Railway System" 
+
+root.appendChild(header)
--- script.js
+++ script.js
@@ -1,9 +1,128 @@
+  let fetch_data = await fetch("https://api-7p3m.onrender.com/users");
+  let file  = await fetch_data.json();
+  
+
+  console.log(file)
+  
+  
+let container = document.createElement("div");
+
+console.log("adsfads")
+
+console.log(container)
+container.className = "block";
+let main =document.createElement("div") ;
+
+let search1 = document.createElement("div");
+search1.className = "Search"; 
+search1.innerText = "Name of the passenger";
+let textBox1 = document.createElement("input");
+textBox1.className = "input";
+textBox1.placeholder = "Name"
+search1.appendChild(textBox1);
+main.appendChild(search1) 
 
 
-let fetch_data = fetch("https://railway-api.free.beeceptor.com/")
-let obj = fetch_data.then((data) =>{
- 
-    return data.json()})
-obj.then((array) => {
-    console.log(array)
-})
+let search2 = document.createElement("div");
+search2.className = "Search"; 
+search2.innerText = "PNR number";
+let textBox2 = document.createElement("input");
+textBox2.className = "input";
+textBox2.placeholder = "Pnr_number"
+search2.appendChild(textBox2);
+main.appendChild(search2)
+
+
+let search3 = document.createElement("div");
+search3.className = "Search"; 
+search3.innerText = "Age";
+let textBox3 = document.createElement("input");
+textBox3.className = "input";
+textBox3.placeholder = "Age"
+search3.appendChild(textBox3);
+main.appendChild(search3)
+
+let search5 = document.createElement("div");
+search5.className = "Search"; 
+search5.innerText = "Train name";
+let textBox5 = document.createElement("input");
+textBox5.className = "input";
+textBox5.placeholder = "Vande Bharat"
+search5.appendChild(textBox5);
+main.appendChild(search5)
+
+let search4 = document.createElement("div");
+search4.className = "Search"; 
+search4.innerText = "Destination";
+let textBox4 = document.createElement("input");
+textBox4.className = "input";
+textBox4.placeholder = "Destination"
+search4.appendChild(textBox4);
+main.appendChild(search4)
+
+
+
+
+let button = document.createElement("button")
+button.className = "button" ; 
+button.innerText = "Search" ; 
+
+main.appendChild(button)
+let root = document.getElementById("root");
+
+let main_container = document.createElement("div") 
+main_container.className = "main_container"
+root.appendChild(main);
+
+
+// let inner1 = document.createElement("div")
+// inner1.className = "inner1" 
+// let inner2 = document.createElement("div")
+// inner2.className = "inner2" 
+// let inner3 = document.createElement("div")
+// inner3.className = "inner3"  
+// let inner4 = document.createElement("div")
+// inner4.className = "inner4" 
+// let inner5 = document.createElement("div")
+// inner5.className = "inner5" 
+// container.appendChild(inner4)
+//     container.appendChild(inner1)
+
+//     container.appendChild(inner2)
+//     container.appendChild(inner5)
+//     container.appendChild(inner3)
+
+// console.log(container)
+
+// console.log(container.innerText)
+// root.appendChild(container);
+file.forEach((element , i ) => {
+
+    let container = document.createElement("div");
+
+    container.className  = "block"
+
+    let inner1 = document.createElement("p");
+    inner1.innerText = element.pnr_number;
+
+    let inner2 = document.createElement("p");
+    inner2.innerText = element.name;
+
+    let inner3 = document.createElement("p");
+    inner3.innerText = element.destination_station;
+
+    let inner4 = document.createElement("p");
+    inner4.innerText = element.train_name;
+
+    let inner5 = document.createElement("p");
+    inner5.innerText = element.age;
+
+    container.append(inner1, inner2, inner3, inner4, inner5);
+
+    main_container.append(container);
+
+});
+console.log(main_container)
+root.appendChild(main_container)
+
+
